5176afbaff56fc2cd5ccd1990096f09a488371fb
Welson Júnior <welsongt9@live.com>
2020-05-19 23:39:26 -0300

    Que comecem os estudos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+/*
+// O componente React aceita apenas uma div dentro dele
+//Se colocar duas dá erro, por padrão tem a abertura de parênteses e 
+// É obrigado a importar o react para a aplicação
+--Componente Funcional--
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       hello world!
     </div>
-  );
+  )
 }
+*/
+/*
+//Outra forma de fazer um componente
+class App extends React.Component {
+    render() {
+    return (
+      <div>
+        hello World 
+      </div>
+    )
+  }
+}
+*/
+/*
+class App extends React.Component {
+  //Possibilidades de se fazer um componente
+  state = {
+    numero1 : null,
+    numero2 : null,
+    resultado: null
+    }
+
+    somar = () => {
+      const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
+      this.setState({resultado: resultado})
+    }
+
+  render() {
+    return (
+      //Referenciando a variavel nome
+      //Estou fazendo de tal forma que a medida que outro evento input ocorra é para atualizar a variável nome também
+      <div>
+        <label>Primeiro:</label>
+        <input type = "text" value = {this.state.numero1} onChange = {(e) => this.setState({numero1: e.target.value})}/><br/>
+        
+        <label>Segundo:</label>
+        <input type = "text" value = {this.state.numero2} onChange = {(e) => this.setState({numero2: e.target.value})}/><br/>
+
+        <button 
+          onClick={this.somar}>
+            Somar
+        </button><br/>
+
+        O resultado é: {this.state.resultado}
+      </div>
+    )
+  }
+}
+*/
+
+import 'bootswatch/dist/flatly/bootstrap.css'
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//React é para single page aplication, também transforma JS em navegadores
+/*
+function Clock(props) {
+    return(
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    )
+    }
+    function tick() {
+    ReactDOM.render(
+        //Estou passando o objeto a ser rendenrizado e onde ele vai ser renderizado
+        //Esse Clock é uma função que eu passei que estou usando ele como um elemento do react
+        <Clock date={new Date()}/>,
+        document.getElementById('root')
+    );
+  }
+  
+  setInterval(tick, 1000);
+----------------------------*/
+//-----------------------------Maneira Ideal----------------------
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//State é privado e totalmente controlado pelo componente é
+//Tipo para atualizar e guarda as novas informações do componente
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Clock extends React.Component {
+   //Fazer o construtor para cetar as informações no objeto e possibilitar novas
+    //Componentes de classes devem sempre chamar o construtor com props.
+   constructor(props){
+       super(props);
+       this.state = {date: new Date()}
+   }
+   //É importante configurar quando o objeto e renderizado pela primeira vez
+   //Mounting
+   //O método componentDidMount() é executado depois que a saída do componente é renderizada no DOM
+   //Estou começando o temporizador
+   componentDidMount(){
+    this.timerID = setInterval(() => 
+        this.tick(), 1000);
+   }
+
+   //E também é importante limpar quando o elemento for removido
+   //ummounting
+   //Estou limpando o temporizador
+   componentWillUnmount(){
+    clearInterval(this.timerID)
+   }
+
+   //Não mudar o setState manualmente, e o único lugar possível atribuir this.state é o construtor
+   //State pode funcionar assincronamente, por isso não se deve confiar no this.props e this.state
+   //Um pode estar fazendo algo e o outro outra, dando erro no código 
+   tick(){
+       this.setState({
+           date: new Date()
+       });
+   }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+
+  //O state pode passar as informações presente neles do elemento pai para o filho
+
+  //todos os componentes estão isolados
+  function App() {
+    return (
+      <div>
+        <Clock />
+        <Clock />
+        <Clock />
+      </div>
+    );
+  }
+
+    ReactDOM.render(
+        //Estou passando o objeto a ser rendenrizado e onde ele vai ser renderizado
+        //Esse Clock é uma função que eu passei que estou usando ele como um elemento do react
+        <App />,
+        document.getElementById('root')
+    );
